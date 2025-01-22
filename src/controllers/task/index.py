1ac1bc9b4cfcaacc0ad7337c48171efd7e3f8ada
St80ene <etienejames5@gmail.com>
2025-01-22 01:29:19 +0100

    chore(refactor): refactored app to use controller method and added files
    Signed-off-by: St80ene <etienejames5@gmail.com>

---
+++ src/controllers/task/index.py
@@ -0,0 +1,62 @@
+from typing import Any
+
+from flask import jsonify, request
+
+from database.db import db_connection, db_cursor, db_init
+
+
+class Task:
+
+    def findAll() -> dict[str, Any]:
+        db_cursor.execute("SELECT * FROM tasks")
+        tasks = db_cursor.fetchall()
+
+        return {"status": 200, "message": "Tasks fetch successful", "tasks": tasks}
+
+    def store():
+
+        try:
+
+            # Check the Content-Type header
+            content_type = request.headers.get("Content-Type", "").lower()
+
+            title = ("",)
+            description = ""
+
+            # Parse data based on Content-Type
+            if "application/json" in content_type:
+                data = request.get_json()
+                title = data.get("title")
+                description = data.get("description", "")
+
+            elif "application/x-www-form-urlencoded" in content_type:
+                title = request.form.get("title")
+                description = request.form.get("description", "")
+                print("2nd: ==>", title, description)
+            else:
+                return (
+                    jsonify(
+                        {
+                            "status": 415,
+                            "message": "Unsupported Media Type. Use 'application/json' or 'application/x-www-form-urlencoded'.",
+                        }
+                    ),
+                    415,
+                )
+
+            # Validate input
+            if not title:
+                return jsonify({"status": 400, "message": "Title is required"}), 400
+
+            print(title, description)
+            db_cursor.execute(
+                "INSERT INTO tasks (title, description) VALUES (?, ?)",
+                (title, description),
+            )
+
+            db_connection.commit()
+            print("To do task successfully added")
+
+            return {"status": 200, "message": "Tasks added successfully"}
+        except Exception as e:
+            return {"status": 500, "message": "An error occured", "error": str(e)}
